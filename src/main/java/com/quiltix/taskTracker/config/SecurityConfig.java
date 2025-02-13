9c629120886a9904e5f1acd270c14a55b4004af5
Quiltix <vanya200632@gmail.com>
2025-02-13 15:47:04 +0300

    finish cors and auth

--- src/main/java/com/quiltix/taskTracker/config/SecurityConfig.java
+++ src/main/java/com/quiltix/taskTracker/config/SecurityConfig.java
@@ -34,7 +34,7 @@ public class SecurityConfig {
         http
 
                 .csrf(AbstractHttpConfigurer::disable)
-
+                .cors(Customizer.withDefaults())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 
                 .authorizeHttpRequests(auth -> auth
