2b52dea1efb3e58f1118ddb88275034feb7534bb
Quiltix <vanya200632@gmail.com>
2025-02-13 18:45:40 +0300

    edit auth controller and create userService

--- src/main/java/com/quiltix/taskTracker/controller/AuthController.java
+++ src/main/java/com/quiltix/taskTracker/controller/AuthController.java
@@ -8,6 +8,7 @@ import com.quiltix.taskTracker.DTO.RegisterRequest;
 import com.quiltix.taskTracker.model.User;
 import com.quiltix.taskTracker.model.UserRepository;
 import com.quiltix.taskTracker.security.JwtTokenProvider;
+import com.quiltix.taskTracker.service.UserService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -33,15 +34,16 @@ public class AuthController {
 
     private final AuthenticationManager authenticationManager;
     private final JwtTokenProvider tokenProvider;
-    private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final UserService userService;
 
 
-    public AuthController(UserRepository userRepository, AuthenticationManager authenticationManager, JwtTokenProvider tokenProvider, UserRepository userRepository1, PasswordEncoder passwordEncoder) {
+    public AuthController(UserService userService, AuthenticationManager authenticationManager, JwtTokenProvider tokenProvider, PasswordEncoder passwordEncoder) {
         this.authenticationManager = authenticationManager;
         this.tokenProvider = tokenProvider;
-        this.userRepository = userRepository1;
+
         this.passwordEncoder = passwordEncoder;
+        this.userService = userService;
     }
 
     @Operation(
@@ -58,29 +60,32 @@ public class AuthController {
     @ApiResponse(responseCode = "400", description = "Пользователь с таким именем уже существует",
             content = @Content(
                     mediaType = "application/json",
-                    schema = @Schema(type = "string",example = "Eror: User with this username is already exists")))
+                    schema = @Schema(type = "string",example = "User with this username is already exists")))
 
+    @ApiResponse(responseCode = "400", description = "Поле username не может быть пустым",
+            content = @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(type = "string",example = "Username cannot be empty")))
+    @ApiResponse(responseCode = "400", description = "Поле password не может быть пустым",
+            content = @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(type = "string",example = "Password cannot be empty")))
     @ApiResponse(responseCode = "500", description = "Ошибка сервера",
             content = @Content(
                     mediaType = "application/json",
                     schema = @Schema(type = "string",example = "Try again later")))
     @PostMapping("/register")
     public ResponseEntity <?> registerUser (@RequestBody RegisterRequest registerRequest){
-
-        if (userRepository.findUserByUsername(registerRequest.getUsername()).isPresent()){
-            return  ResponseEntity
-                    .badRequest()
-                    .body("Eror: User with this username is already exists");
+        try {
+            String message = userService.registerUser(registerRequest);
+            return ResponseEntity.ok().body(message);
         }
-
-        User user = new User();
-        user.setUsername(registerRequest.getUsername());
-        user.setPassword(passwordEncoder.encode(registerRequest.getPassword()));
-        userRepository.save(user);
-
-        return ResponseEntity
-                .ok()
-                .body("The user has been successfully registered");
+        catch (IllegalArgumentException ex){
+            return ResponseEntity.badRequest().body(ex.getMessage());
+        }
+        catch (Exception ex){
+            return ResponseEntity.status(500).body(ex.getMessage());
+        }
     }
 
     @Operation(
@@ -93,10 +98,18 @@ public class AuthController {
                     mediaType = "application/json",
                     schema = @Schema(implementation = JwtAuthenticationResponse.class)))
 
-    @ApiResponse(responseCode = "400", description = "Неверный логин или пароль",
+    @ApiResponse(responseCode = "400", description = "Логин или пароль пустой",
             content = @Content(
                     mediaType = "application/json",
-                    schema = @Schema(type = "string",example = "Invalid username or password")))
+                    schema = @Schema(type = "string",example = "Username cannot be empty")))
+    @ApiResponse(responseCode = "400", description = "Логин или пароль пустой",
+            content = @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(type = "string",example = "Password cannot be empty")))
+    @ApiResponse(responseCode = "400", description = "Неправильный пароль",
+            content = @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(type = "string",example = "Invalid password")))
 
     @ApiResponse(responseCode = "500", description = "Ошибка сервера",
             content = @Content(
@@ -104,36 +117,17 @@ public class AuthController {
                     schema = @Schema(type = "string",example = "Try again later")))
     @PostMapping("/login")
     public ResponseEntity<?> authenticateUser(@RequestBody LoginRequest loginRequest) {
+
         try {
-            if( userRepository.findUserByUsername(loginRequest.getUsername()).isEmpty()){
-                throw new UsernameNotFoundException("User not found");
-
-            }
-            // 1. Создаем объект аутентификации
-            UsernamePasswordAuthenticationToken authToken =
-                    new UsernamePasswordAuthenticationToken(loginRequest.getUsername(), loginRequest.getPassword());
-
-            // 2. Аутентифицируем пользователя
-            Authentication authentication = authenticationManager.authenticate(authToken);
-
-            // 3. Устанавливаем аутентификацию в SecurityContext
-            SecurityContextHolder.getContext().setAuthentication(authentication);
-
-            // 4. Генерируем JWT-токен
-            String jwt = tokenProvider.generateToken(authentication);
-
-            // 5. Отправляем успешный ответ
-            return ResponseEntity.ok(new JwtAuthenticationResponse(jwt));
-
-        } catch (UsernameNotFoundException ex) {
-            return ResponseEntity
-                    .badRequest()
-                    .body("Invalid username or password");
-        } catch (Exception ex) {
-            return ResponseEntity
-                    .internalServerError()
-                    .body("Try again later");
+            String token = userService.authenticateUser(loginRequest);
+            return ResponseEntity.ok().body(new JwtAuthenticationResponse(token));
         }
+        catch (UsernameNotFoundException | IllegalArgumentException | BadCredentialsException ex){
+            return ResponseEntity.badRequest().body(ex.getMessage());
+        } catch (Exception ex){
+            return ResponseEntity.status(500).body(ex.getMessage());
+        }
+
     }
 
 }
---
+++ src/main/java/com/quiltix/taskTracker/service/UserService.java
@@ -0,0 +1,78 @@
+package com.quiltix.taskTracker.service;
+
+import com.quiltix.taskTracker.DTO.LoginRequest;
+import com.quiltix.taskTracker.DTO.RegisterRequest;
+import com.quiltix.taskTracker.model.User;
+import com.quiltix.taskTracker.model.UserRepository;
+import com.quiltix.taskTracker.security.JwtTokenProvider;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
+    private final JwtTokenProvider jwtTokenProvider;
+
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder, AuthenticationManager authenticationManager, JwtTokenProvider jwtTokenProvider) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.authenticationManager = authenticationManager;
+        this.jwtTokenProvider = jwtTokenProvider;
+    }
+
+    @Transactional
+    public String registerUser(RegisterRequest registerRequest) {
+        if (registerRequest.getUsername() == null || registerRequest.getUsername().isEmpty()) {
+            throw new IllegalArgumentException("Username cannot be empty");
+        }
+        if (registerRequest.getPassword() == null || registerRequest.getPassword().isEmpty()) {
+            throw new IllegalArgumentException("Password cannot be empty");
+        }
+        if (userRepository.findUserByUsername(registerRequest.getUsername()).isPresent()) {
+            throw new IllegalArgumentException("User with this username already exists");
+        }
+        User user = new User();
+        user.setUsername(registerRequest.getUsername());
+        user.setPassword(passwordEncoder.encode(registerRequest.getPassword()));
+        userRepository.save(user);
+
+        return "The user has been successfully registered";
+
+    }
+
+    public String authenticateUser(LoginRequest loginRequest) {
+        if (loginRequest.getUsername() == null || loginRequest.getUsername().isEmpty()) {
+            throw new IllegalArgumentException("Username cannot be empty");
+        }
+        if (loginRequest.getPassword() == null || loginRequest.getPassword().isEmpty()) {
+            throw new IllegalArgumentException("Password cannot be empty");
+        }
+        if(userRepository.findUserByUsername(loginRequest.getUsername()).isEmpty()){
+            throw new UsernameNotFoundException("User not found");
+        }
+        UsernamePasswordAuthenticationToken authToken =
+                new UsernamePasswordAuthenticationToken(loginRequest.getUsername(), loginRequest.getPassword());
+        Authentication authentication;
+        try {
+            authentication = authenticationManager.authenticate(authToken);
+        } catch (BadCredentialsException ex) {
+            throw new BadCredentialsException("Invalid password");
+        }
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        return jwtTokenProvider.generateToken(authentication);
+    }
+}
