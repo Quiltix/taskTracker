6fdb0a4ed36db5236b118e482656856223568cf0
Quiltix <vanya200632@gmail.com>
2025-04-03 17:13:33 +0300

    feat: add creating dir in main java file

--- src/main/java/com/quiltix/tasktracker/TaskTrackerApplication.java
+++ src/main/java/com/quiltix/tasktracker/TaskTrackerApplication.java
@@ -1,17 +1,30 @@
 package com.quiltix.tasktracker;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.TimeZone;
 
 
 @EnableScheduling
 @SpringBootApplication
-public class TaskTrackerApplication {
+public class TaskTrackerApplication implements CommandLineRunner {
+
+	@Value("${app.avatar-dir}")
+	private String avatarDir;
+
 	public static void main(String[] args) {
 		TimeZone.setDefault(TimeZone.getTimeZone("Europe/Moscow"));
 		SpringApplication.run(TaskTrackerApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Files.createDirectories(Paths.get(avatarDir));
 	}
 }
